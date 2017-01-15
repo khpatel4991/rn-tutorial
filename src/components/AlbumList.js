fe776f3f4d6addaa88aa157fc5e36726781babe5
Kashyap Patel <khpatel4991@gmail.com>
2017-01-14 23:21:30 -0500

    Removed axios, using native fetch method. Till: Section c7, Lec 34. Getting data from dummy source

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
-//import axios from 'axios';
 
 class AlbumList extends Component {
 
+    state = {
+        albums: []
+    };
+
     componentWillMount() {
-        //axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-          //  .then(console.log, console.log);
+        fetch('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(res => res.json())
+        .then(resJson => {
+            this.setState({ albums: resJson });
+        });
     }
 
     render() {
+        console.log(this.state);
         return (
             <View>
                 <Text>Album List!!!</Text>
